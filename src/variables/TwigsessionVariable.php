3d078aa1db670e993e11d44ad7e65ed87a52cbd7
Jhordan <jhordangabriel@hotmail.com>
2018-03-18 22:48:25 -0300

    sync

--- src/variables/TwigsessionVariable.php
+++ src/variables/TwigsessionVariable.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Twigsession plugin for Craft CMS 3.x
+ * Twig Session plugin for Craft CMS 3.x
  *
- * Twigsession
+ * Twig session
  *
  * @link      https://github.com/jhordangab/
  * @copyright Copyright (c) 2018 jhordangab
@@ -10,31 +10,47 @@
 
 namespace jhordangab\twigsession\variables;
 
-use jhordangab\twigsession\Twigsession;
+use jhordangab\twigsession\TwigSession;
 
 use Craft;
 
 /**
- * Twigsession Variable
+ * Twig Session Variable
  *
  * Craft allows plugins to provide their own template variables, accessible from
- * the {{ craft }} global variable (e.g. {{ craft.twigsession }}).
+ * the {{ craft }} global variable (e.g. {{ craft.twigSession }}).
  *
  * https://craftcms.com/docs/plugins/variables
  *
  * @author    jhordangab
- * @package   Twigsession
+ * @package   TwigSession
  * @since     1.0.0
  */
-class TwigsessionVariable
+class TwigSessionVariable
 {
-    public function set($key, $value)
-    {
-      Craft::$app->getSession()->set($key, $value);
-    }
+    // Public Methods
+    // =========================================================================
 
-    public function get($key)
+    /**
+     * Whatever you want to output to a Twig template can go into a Variable method.
+     * You can have as many variable functions as you want.  From any Twig template,
+     * call it like this:
+     *
+     *     {{ craft.twigSession.exampleVariable }}
+     *
+     * Or, if your variable requires parameters from Twig:
+     *
+     *     {{ craft.twigSession.exampleVariable(twigValue) }}
+     *
+     * @param null $optional
+     * @return string
+     */
+    public function exampleVariable($optional = null)
     {
-      Craft::$app->getSession()->get($key);
+        $result = "And away we go to the Twig template...";
+        if ($optional) {
+            $result = "I'm feeling optional today...";
+        }
+        return $result;
     }
 }
